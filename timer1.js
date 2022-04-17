70dfaf6cbd4b71516939ce043e716abd5730d2bf
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-17 17:26:06 +0000

    added comments to code

--- timer1.js
+++ timer1.js
@@ -1,12 +1,14 @@
 const times = process.argv.slice(2);
 const arr = [];
 
+//filter out any negative numbers and NaN values
 for (let time of times) {
   if (Math.sign(time) === 1) {
     arr.push(time);
   }
 };
 
+//beep at every second specified in the loop;
 for (let element of arr) {
   setTimeout(()=> {
     process.stdout.write('\x07');
